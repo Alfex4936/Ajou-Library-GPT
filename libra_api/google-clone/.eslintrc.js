479da1c2be5e4e5f29747b3149ea375e944c3772
Alfex4936 <ikr@kakao.com>
2025-05-30 18:43:08 +0900

    fix for vercel

---
+++ libra_api/google-clone/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  }
+};
